73e460ebbc4fdf0aee9f4f78a4805030c393cf84
Bruce Collie <brucecollie82@gmail.com>
2022-01-06 22:04:18 +0000

    Fix bugs

--- wordle.py
+++ wordle.py
@@ -13,17 +13,17 @@ def character_count(words):
     return count
 
 def score(word, counts):
-    return sum([counts[c] for c in set(word)])
+    return sum([counts[c] for c in word])
 
 def compatible(word, greys, yellows, greens):
-    if not set(word).isdisjoint(set(greys)):
-        return False
-
-    if set(word) & set(yellows) != set(yellows):
+    if not set(yellows) <= set(word):
         return False
 
     for idx in greens:
         if word[idx] != greens[idx]:
+            return False
+
+        if idx in greys and word[idx] == greys[idx]:
             return False
 
     return True
@@ -32,13 +32,13 @@ def compatible(word, greys, yellows, greens):
 # x -> yellow
 # # -> green
 def parse_response(word, resp):
-    greys = []
+    greys = {}
     yellows = []
     greens = {}
 
     for idx, c in enumerate(word):
         if resp[idx] == '.':
-            greys.append(c)
+            greys[idx] = c
         elif resp[idx] == 'x':
             yellows.append(c)
         elif resp[idx] == '#':
@@ -51,23 +51,22 @@ def parse_response(word, resp):
 if __name__ == '__main__':
     words = initial_words()
     
+    blacklist = set()
+
     while True:
         counts = character_count(words)
         ranking = sorted(words, key=lambda word: score(word, counts), reverse=True)
 
-        blacklist = set()
-
         choice = None
         for offer in ranking:
-            if offer not in blacklist:
-                print(f"try: {offer}")
-                resp = input("> ")
+            print(f"try: {offer}")
+            resp = input("> ")
 
-                if len(resp) > 0:
-                    choice = offer
-                    break
-                else:
-                    blacklist.add(offer)
+            blacklist.add(offer)
+
+            if len(resp) > 0:
+                choice = offer
+                break
 
         parsed = parse_response(choice, resp)
-        words = list(filter(lambda w: compatible(w, *parsed), words))
+        words = list(filter(lambda w: compatible(w, *parsed) and w not in blacklist, words))
